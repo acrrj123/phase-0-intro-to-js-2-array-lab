d53b02d430142b24443380be0119521bfc970b01
Anna Cole <acrrj123@gmail.com>
2023-02-14 16:20:34 -0600

    commit tree 66db0b3a412cf9c6bd4143c92706baa986392629

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+
+    const cats = ["Milo", "Otis", "Garfield"];
+
+    function destructivelyAppendCat(name) {
+        cats.push("Ralph");
+    }
+    function destructivelyPrependCat(name) {
+        cats.unshift("Bob");
+    }
+    function destructivelyRemoveLastCat() {
+        cats.pop();
+    }
+    function destructivelyRemoveFirstCat() {
+        cats.shift();
+    }
+    function appendCat(name) {
+        return name = [...cats, "Broom"];
+    }
+    function prependCat(name) {
+        return name = ["Arnold", ...cats];
+    }
+    function removeLastCat() {
+        return name = cats.slice(0, 2);
+    }
+    function removeFirstCat() {
+        return name = cats.slice(1);
+    }
+    
+
+    
